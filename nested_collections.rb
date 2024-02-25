92dfe81639737083a2c0cae4f61c070305812dc3
Hossain <arafat.kd99@gmail.com>
2024-02-25 21:58:57 +0100

    Add dig method to access non-exitent element and  avoid NoMethodError

--- nested_collections.rb
+++ nested_collections.rb
@@ -11,3 +11,6 @@ teacher_mailboxes = [
   puts teacher_mailboxes[2][2] # "Iverson"
   puts teacher_mailboxes[3][0] # NoMethodError: undefined method `[]' for nil:NilClass
   puts teacher_mailboxes[0][3] # nil
+  # dig METHOD TO ACCESS NON-EXISTENT ELEMENTS TO AVOID NoMethodERROR
+  puts teacher_mailboxes.dig(3, 0) # nil
+  puts teacher_mailboxes.dig(0, 3) # nil
