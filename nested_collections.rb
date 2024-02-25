9b5d7c58a4ed24ce5e2447c1e269a8f6a52e4879
Hossain <arafat.kd99@gmail.com>
2024-02-25 21:57:19 +0100

    Access to nested array

---
+++ nested_collections.rb
@@ -0,0 +1,13 @@
+# NESTEED ARRAY
+test_scores = [[90, 95, 99], [80, 85, 89], [70, 75, 79]]
+teacher_mailboxes = [
+  ["Adam", "Baker", "Carter"],
+  ["Davis", "Erickson", "Fisher"],
+  ["Garcia", "Harris", "Iverson"]
+]
+  # Accessing elements in a nested array
+  puts test_scores[0][0] # 90
+  puts teacher_mailboxes[0][0] # "Adam"
+  puts teacher_mailboxes[2][2] # "Iverson"
+  puts teacher_mailboxes[3][0] # NoMethodError: undefined method `[]' for nil:NilClass
+  puts teacher_mailboxes[0][3] # nil
